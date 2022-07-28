c05ccf86e0afa0fa75b5f45c51a38eb49a0e135c
Meow <76005806+gabmeow@users.noreply.github.com>
2022-07-28 19:53:28 +0200

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,15 +1,33 @@
-void write_string( int colour, const char *string )
+#include<stdbool.h>
+#include<stdint.h>
+#include<stddef.h>
+
+void WriteCharacter(unsigned char c, unsigned char forecolour, unsigned char backcolour, int x, int y)
 {
-    volatile char *video = (volatile char*)0xB8000;
-    while( *string != 0 )
+     uint16_t attrib = (backcolour << 4) | (forecolour & 0x0F);
+     volatile uint16_t * where;
+     where = (volatile uint16_t *)0xB8000 + (y * 80 + x) ;
+     *where = c | (attrib << 8);
+}
+
+void write_string(int colour, const char *string, unsigned char backcolour, int x, int y, int char_num)
+{
+    volatile char *video = (volatile char*)0xB8000;         // VGA
+    uint16_t attrib = (backcolour << 4) | (colour & 0x0f);  //set background and text color
+    volatile uint16_t * where;
+    int i = 0;                          // counter
+    while (i < char_num)                // algorithm that takes every character of the list and prints it
     {
-        *video++ = *string++;
-        *video++ = colour;
+        where = (volatile uint16_t *) 0xB8000 + (y * 80 + x + i);
+        *where = *string++ | (attrib << 8);
+        i++;
     }
 }
 
 _main()
 {
-    const char string[5] = {'H', 'e', 'l', 'l', 'o'};
-    write_string(0x0f, string);
+    const char cubix[5] = {'C', 'u', 'b', 'i', 'x'};
+    write_string(0x0f, cubix, 0x00, 0, 0, 5);           // prints
+    const char version[6] = {'V', '0', '0', '1', 'A', '5'};
+    write_string(0x0f, version, 0x00, 0, 1, 6);         // prints
 }
